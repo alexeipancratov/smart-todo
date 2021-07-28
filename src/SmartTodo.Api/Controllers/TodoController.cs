fde22bdee3fbd2397b8fdcd49b21e5cb73170dcb
Alexei Pancratov <drivergear@gmail.com>
2021-07-28 10:17:49 -0400

    Add project files.

---
+++ src/SmartTodo.Api/Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmartTodo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartTodo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("todos")]
+        public IEnumerable<TodoItem> Get()
+        {
+            return new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Title = "Wash dishes" }
+            };
+        }
+
+        [HttpGet("todo")]
+        public TodoItem Get(string id)
+        {
+            var items = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Title = "Wash dishes" }
+            };
+
+            return items.FirstOrDefault(i => string.Equals(i.Id, id, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ src/SmartTodo.Domain/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmartTodo.Domain
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime? DateTimeCreated { get; set; }
+
+        public DateTime? DateTimeCompleted { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
